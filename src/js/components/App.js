9e84d40fb492e49d0f698b41be8314ba424ed1b1
Dev bohre <devendra.bhumca2014@gmail.com>
2018-05-10 16:14:47 +0530

    first commit

---
+++ src/js/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import List from "./List";
+import Form from "./Form";
+
+console.log("list in app.js", List);
+const App = () => (
+  <div className="row" style={{'marginRight': 0, 'marginLeft': 0}}>
+    <div className="col-md-1"></div>
+    <div className="col-md-4">
+      <h2>Articles</h2>
+      <List />
+    </div>
+    <div className="col-md-1"></div>
+    <div className="col-md-1"></div>
+    <div className="col-md-4">
+      <h2>Add a new article</h2>
+      <Form />
+    </div>
+    <div className="col-md-1"></div>
+  </div>
+);
+
+export default App;
---
+++ src/js/components/List.js
@@ -0,0 +1,18 @@
+// src/js/components/List.js
+import React from "react";
+import { connect } from "react-redux";
+const mapStateToProps = state => {
+  return { articles: state.articles };
+};
+console.log("articles", mapStateToProps)
+const ConnectedList = ({ articles }) => (
+  <ul className="list-group list-group-flush">
+    {articles.map(el => (
+      <li className="list-group-item" key={el.id}>
+        {el.title}
+      </li>
+    ))}
+  </ul>
+);
+const List = connect(mapStateToProps)(ConnectedList);
+export default List;
