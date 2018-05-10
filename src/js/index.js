eff88a61fca992294cf752c4108e2205a603084a
Dev bohre <devendra.bhumca2014@gmail.com>
2018-05-10 16:46:22 +0530

    updated successfully

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,6 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store/index";
 import App from "./components/App";
-window.store = store
-console.log("state in store", store.getState())
 render(
   <Provider store={store}>
     <App />
